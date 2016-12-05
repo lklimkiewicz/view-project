645646b2394550536a62cd75f500e58b4d511f7c
lklimkiewicz <lukaszklimkiewicz@mac-mini-ukasz.local>
2016-12-05 20:59:51 +0100

    Initial Commit

---
+++ viewProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  viewProject
+//
+//  Created by Łukasz Klimkiewicz on 05.12.2016.
+//  Copyright © 2016 Łukasz Klimkiewicz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
