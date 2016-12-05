dd59fe63c220f41542a67bb85aec12253cee0e88
lklimkiewicz <lukaszklimkiewicz@Mac-mini-ukasz.local>
2016-12-05 21:49:43 +0100

    xib

---
+++ viewProject/FeatureView.swift
@@ -0,0 +1,19 @@
+//
+//  FeatureView.swift
+//  viewProject
+//
+//  Created by Łukasz Klimkiewicz on 05.12.2016.
+//  Copyright © 2016 Łukasz Klimkiewicz. All rights reserved.
+//
+
+import UIKit
+
+class FeatureView: UIView {
+
+    @IBOutlet weak var featureTitle: UILabel!
+    @IBOutlet weak var featureButton: UIButton!
+
+    
+    
+    
+}
--- viewProject/ViewController.swift
+++ viewProject/ViewController.swift
@@ -15,11 +15,33 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        exampleView.backgroundColor = UIColor.red
+        //exampleView.backgroundColor = UIColor.red
         //
         
-        self.view.addSubview(exampleView)
+        if let featuredView2 = Bundle.main.loadNibNamed("Feature", owner: self, options: nil)?.first as? FeatureView {
+            featuredView2.featureTitle.text = "testowy tytul"
+            featuredView2.featureButton.addTarget(self, action: #selector(ViewController.buyButtonPressed(sender:)), for:.touchUpInside)
+            self.view.addSubview(featuredView2)
+        }
         
+        
+        if let featuredView3 = Bundle.main.loadNibNamed("Feature", owner: self, options: nil)?.first as? FeatureView {
+            featuredView3.frame.origin.y = 400
+            featuredView3.backgroundColor = UIColor.red
+            featuredView3.featureTitle.text = "testowy tytul 2"
+            self.view.addSubview(featuredView3)
+        }
+        
+        
+        
+        
+       //self.view.addSubview(exampleView)
+        
+    }
+    
+    
+    func buyButtonPressed(sender : UIButton){
+        print("cool button pressed")
     }
 
     override func didReceiveMemoryWarning() {
